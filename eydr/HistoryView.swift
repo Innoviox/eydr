48ee40b9aa32604739d9f16a13672004363ef964
Simon Chervenak <sarras305@gmail.com>
2021-01-22 10:26:14 -0500

    starting bars

--- eydr/HistoryView.swift
+++ eydr/HistoryView.swift
@@ -8,6 +8,19 @@
 import SwiftUI
 import CoreData
 
+func makeBarHeights(_ items: [Item], getter: (Item) -> Int) -> [Int] {
+    let heights = items.map(getter)
+    
+    let min = heights.min()!, max = heights.max()!
+    
+    
+    return heights.map { ($0 - min) / (max - min) }
+}
+
+func Bar(_ height: Int) -> some View {
+
+}
+
 struct HistoryView: View {
     @Environment(\.calendar) var calendar
     @Environment(\.managedObjectContext) private var viewContext
@@ -22,16 +35,20 @@ struct HistoryView: View {
         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Item")
         do {
             let fetched = try viewContext.fetch(fetchRequest) as! [Item]
+            
+            let topHeights = makeBarHeights(fetched) { Int($0.morning + $0.afternoon) }
+            let botHeights = makeBarHeights(fetched) { Int($0.steps) }
 
             return AnyView(HStack {
-                ForEach(fetched, id: \.self) { item in
+                ForEach(1...fetched.count, id: \.self) { i in
+                    let item = fetched[i]
                     VStack {
-                        Text("\(item.morning + item.afternoon)")
+                        Bar(topHeights[i])
                         Text("\(item.timestamp!.get(.day))")
                             .padding(8)
                             .background(Color.blue)
                             .cornerRadius(8)
-                        Text("\(item.steps)")
+                        Bar(botHeights[i])
                     }
                 }
             })
